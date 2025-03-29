5cb59c671e8a532fb0eb47abab8ac0c02bfe682b
eileen <e0772875@u.nus.edu>
2025-03-29 21:18:27 +0800

    Edit EDA and add feature engineering file

--- eda_ftr_eng/EDA.py
+++ eda_ftr_eng/EDA.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 # Replace empty string, empty list as NaN
 def replace_empty_values(x):
     if isinstance(x, str) and x.strip() == '':
@@ -12,8 +15,21 @@ def replace_empty_values(x):
         return x
 
 def eda(df):
+    df = df[df['Country'] == 'us']
+    df = df.drop(['State','Country'], axis=1)
+    
     df = df.map(lambda x: replace_empty_values(x))
+
+    # These columns are very significant when unspecified/specified
+    df['Salary_Specified'] = df['Range_of_Salary'].notna()
+    df['City_Specified'] = df['City'].notna()
 
     # Replace null values with 'Unspecified' category
     df = df.fillna("Unspecified")
+
+    #Inpute with median to instead of replacing with 0 to prevent increasing bias
+    df['Range_of_Salary'] = pd.to_numeric(df['Range_of_Salary'], errors='coerce')
+    median_salary = df['Range_of_Salary'].median()
+    df['Range_of_Salary'] = df['Range_of_Salary'].fillna(median_salary).astype(int)
+
     return df
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import sys
 sys.path.append(str(Path().resolve().parent))
 from data_preprocessing.preprocessing import preprocessing
 from eda_ftr_eng.EDA import eda
+from eda_ftr_eng.feature_engineering import feature_engineering
 
 df_raw = pd.read_csv('Data/Raw/Job_Frauds.csv', encoding='latin-1')
 preprocessed_df = preprocessing(df_raw)
 #then rebalancing - tbc
 eda_df = eda(preprocessed_df)
-# feature engineering
+ftr_eng_df = feature_engineering(eda_df)
 # feature selection
 # modelling
