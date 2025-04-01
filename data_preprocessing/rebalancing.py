db121dbb999b126bd3d62eafd31e91b0b778ac58
ShiLing <e0772999@u.nus.edu>
2025-04-01 11:00:38 +0800

    edits

--- data_preprocessing/rebalancing.py
+++ data_preprocessing/rebalancing.py
@@ -1,7 +1,15 @@
+import pandas as pd
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import RandomOverSampler
+
 def oversampling(df):
+    ros = RandomOverSampler(random_state=0)
+    X_resampled, y_resampled = ros.fit_resample(df.drop(columns=['Fraudulent']), df['Fraudulent'])
+    df_oresampled = pd.concat([X_resampled, y_resampled], axis=1)
+    return df_oresampled
 
-    return 
-
-
-
-!pip install imblearn
+def undersampling(df):
+    rus = RandomUnderSampler(random_state=0)
+    X_resampled, y_resampled = rus.fit_resample(df.drop(columns=['Fraudulent']), df['Fraudulent'])
+    df_uresampled = pd.concat([X_resampled, y_resampled], axis=1)
+    return df_uresampled
