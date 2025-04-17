5cc94377abd0682b8cd20a5f1e22279d984b758b
cheryl tan <e0774478@u.nus.edu>
2025-04-17 13:09:55 +0800

    edited

--- main.py
+++ main.py
@@ -2,16 +2,13 @@ import pandas as pd
 from pathlib import Path
 import sys
 sys.path.append(str(Path().resolve().parent))
-from data_preprocessing.preprocessing import preprocessing
+from src.data.preprocessing import preprocessing
 from src.data.EDA import eda
 from src.features.feature_engineering import feature_engineering
 from src.features.feature_selection import feature_selection
 
 df_raw = pd.read_csv('Data/Raw/Job_Frauds.csv', encoding='latin-1')
 preprocessed_df = preprocessing(df_raw)
-#then rebalancing - tbc
 eda_df = eda(preprocessed_df)
 ftr_eng_df = feature_engineering(eda_df)
 feature_selection_df = feature_selection(ftr_eng_df)
-
-# modelling
